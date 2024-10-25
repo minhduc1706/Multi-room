0391d9c49bc81f1bd27e3b4d7fdeb9c82c0540df
minhduc1706 <minhduc0879395@gmail.com>
2024-10-26 00:37:01 +0700

    create a player

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const canvas = document.querySelector("canvas")
+const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
-
 
 canvas.width = 64 * 16;
 canvas.height = 64 * 9;
 
-c.fillStyle = "white";
-c.fillRect(0, 0, canvas.width, canvas.height);
+
+
+const player = new Player();
+
+const animate = () => {
+  window.requestAnimationFrame(animate);
+  c.fillStyle = "white";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  player.draw();
+  player.update();
+};
+
+animate();
