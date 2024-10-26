7c74aa4770adcec128bfe6b17bfacebc768b52e7
minhduc1706 <minhduc0879395@gmail.com>
2024-10-26 22:37:27 +0700

    add background

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const c = canvas.getContext("2d");
 canvas.width = 64 * 16;
 canvas.height = 64 * 9;
 
+
+const backgroundLevel1 = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  imgSrc: "./img/backgroundLevel1.png",
+});
+
 const player = new Player();
 
 const keys = {
@@ -20,8 +29,8 @@ const keys = {
 
 const animate = () => {
   window.requestAnimationFrame(animate);
-  c.fillStyle = "white";
-  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  backgroundLevel1.draw();
 
   player.velocity.x = 0;
   if (keys.d.pressed) {
@@ -34,4 +43,3 @@ const animate = () => {
 };
 
 animate();
-
